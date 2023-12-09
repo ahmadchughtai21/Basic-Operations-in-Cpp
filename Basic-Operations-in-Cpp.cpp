03cf48e204c53a01c7d25e2cbae4658d5174eec3
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2023-12-09 17:44:59 +0500

    Initial commit

---
+++ Basic-Operations-in-Cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    A = 100; // Random initialization  
+    B = A - 98;
+    A += 1;
+
+    // Verification
+    cout << "A: " << A << endl;
+    cout << "B: " << B << endl;
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Basic-Operations-in-Cpp
+ C++ code declares A and B, performs arithmetic operations, prints values.
